58e3d97bbf0e2f240bbeabbaea5255355b6375f6
Pedro Gabriel <pedrogabrielsantoslima@hotmail.com>
2021-02-22 20:09:08 -0300

    commit inicial

---
+++ src/main/java/br/com/desafio/zup/DesafioZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/zup/DesafioZupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.zup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
