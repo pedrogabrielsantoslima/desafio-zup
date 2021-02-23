6e66e94ccf19b611b7d322ad6d6f78e2a9da84a7
Pedro Gabriel <pedrogabrielsantoslima@hotmail.com>
2021-02-22 23:09:43 -0300

    Criacao dos DTOs

--- src/main/java/br/com/desafio/zup/dto/UsuarioDTO.java
+++ src/main/java/br/com/desafio/zup/dto/UsuarioDTO.java
@@ -1,4 +1,71 @@
 package br.com.desafio.zup.dto;
 
-public class UsuarioDTO {
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class UsuarioDTO implements Serializable {
+
+    private static final long serialVersionUID = 7518596221838559448L;
+    private Integer id;
+
+    @NotBlank
+    @Length(max = 60)
+    private String nome;
+
+    @Email
+    @Length(max = 60)
+    private String email;
+
+    @CPF
+    @Length(max = 14)
+    private String cpf;
+
+    @NotNull
+    private LocalDate dataNascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/main/java/br/com/desafio/zup/dto/VacinaDTO.java
+++ src/main/java/br/com/desafio/zup/dto/VacinaDTO.java
@@ -1,4 +1,56 @@
 package br.com.desafio.zup.dto;
 
-public class VacinaDTO {
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class VacinaDTO implements Serializable {
+
+    private static final long serialVersionUID = 8981226123159932541L;
+    private Integer id;
+
+    @NotBlank
+    @Length(max = 60)
+    private String nome;
+
+    @NotNull
+    private LocalDate dataAplicacao;
+
+    @NotNull
+    private Integer idUsuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataAplicacao() {
+        return dataAplicacao;
+    }
+
+    public void setDataAplicacao(LocalDate dataAplicacao) {
+        this.dataAplicacao = dataAplicacao;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 }
